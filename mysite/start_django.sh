04e9a5cce9e5979c5f6f2b02f3c4a406acdcd249
Yuchen Xiong <xiongyuc@bc.edu>
2025-01-30 15:29:17 -0500

    01/30 start_django.sh updated

--- mysite/start_django.sh
+++ mysite/start_django.sh
@@ -1,3 +1,3 @@
+#!/bin/bash
 source /home/yuchen/myprojectenv/bin/activate
-python3 /home/yuchen/Django_database/mysite/manage.py import_data
 exec python3 /home/yuchen/Django_database/mysite/manage.py runserver 0.0.0.0:8000
